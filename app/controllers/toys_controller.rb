cc81ee3a45fed6425584dffba4c3461e9e1273eb
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-28 18:43:52 +0300

    Completed Assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,23 +1,27 @@
 class ToysController < ApplicationController
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_toy_not_found
   wrap_parameters format: []
 
   def index
     toys = Toy.all
-    render json: toys
+    render json: toys, status: :ok
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
+    toy = Toy.find(params[:id])
     toy.update(toy_params)
+    render json: toy, status: :ok
   end
 
+
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = Toy.find(params[:id])
     toy.destroy
     head :no_content
   end
@@ -27,5 +31,9 @@ class ToysController < ApplicationController
   def toy_params
     params.permit(:name, :image, :likes)
   end
+
+  def render_toy_not_found
+		render json: {error: "Toy not found!"}, status: :not_found
+	end
 
 end
